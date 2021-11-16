cbf05a3607bca7a8832dcdff6d2a08e4d707a2e2
Idan <idanlevy2020@gmail.com>
2021-11-17 00:15:33 +0200

    add StoreProvider to App.js commit

--- src/application/App.js
+++ src/application/App.js
@@ -2,21 +2,12 @@ import "./App.css";
 import Header from "../layout/Header/Header";
 import Main from "../layout/Main/Main";
 import Footer from "../layout/Footer/Footer";
-import data from "../components/data";
-// import {useState,createContext} from "react";
-// export const StoreContext = createContext();
 
 import StoreProvider from "../contexts/StoreContext";
 
 function App() {
    return (
       <div className="App">
-         {/* <CartContext.Provider value={value}>
-            <Header/>
-            <Main />
-            <Footer />
-         </CartContext.Provider> */}
-
          <StoreProvider>
             <Header/>
             <Main />
