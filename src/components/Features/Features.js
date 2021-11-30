39d1156004f116a1a80ccf935617f0c43eba568e
Idan <idanlevy2020@gmail.com>
2021-11-30 13:00:41 +0200

    add Features Component

---
+++ src/components/Features/Features.js
@@ -0,0 +1,30 @@
+import "./Features.css";
+
+function Features() {
+   return (
+      <div className="Features">
+      <h1>FEATURES</h1>
+      <div className="FeaturesContent">
+         <p className="p1">
+            As the headphones all others are measured against, the XX99
+            Mark I demonstrates over five decades of audio expertise,
+            redefining the critical listening experience. This pair of
+            closed-back headphones are made of industrial,
+            aerospace-grade materials to emphasize durability at a
+            relatively light weight of 11 oz.
+         </p>
+         <p className="p2">
+            From the handcrafted microfiber ear cushions to the robust
+            metal headband with inner damping element, the components
+            work together to deliver comfort and uncompromising sound.
+            Its closed-back design delivers up to 27 dB of passive
+            noise cancellation, reducing resonance by reflecting sound
+            to a dedicated absorber. For connectivity, a specially
+            tuned cable is included with a balanced gold connector.
+         </p>
+      </div>
+   </div>
+   )
+}
+
+export default Features;
