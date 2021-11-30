a71ff9be4d924452885090f8a54d5d87ee4d5f99
Idan <idanlevy2020@gmail.com>
2021-11-30 13:16:05 +0200

    change css and fix bugs

--- src/pages/ProductDetails/ProductDetails.js
+++ src/pages/ProductDetails/ProductDetails.js
@@ -1,28 +1,38 @@
-import "./ProductDetails.css"
-import React, { useState , useContext } from 'react';
-import CategoriesShops from "../../components/CategoriesShops/CategoriesShops"
+import "./ProductDetails.css";
+import React, { useState, useContext } from "react";
+import CategoriesShops from "../../components/CategoriesShops/CategoriesShops";
 import AboutUs from "../../components/AboutUs/AboutUs";
+import Features from "../../components/Features/Features";
 import Button from "../../components/Button/Button";
 import InTheBox from "../../components/InTheBox/InTheBox";
 import SummaryModal from "../../components/SummaryModal/SummaryModal";
-import {StoreContext} from "../../contexts/StoreContext";
+import { StoreContext } from "../../contexts/StoreContext";
+import { useParams } from "react-router-dom";
+import ProductImages from "../../components/ProductImages/ProductImages";
 
 function ProductDetails(props) {
-   const value=useContext(StoreContext);
-   const addtoCart=value.addtoCart;
+   const value = useContext(StoreContext);
+   const addtoCart = value.addtoCart;
+
+   const params = useParams();
+   const product = value.products.find(
+      (product) => product.id === parseInt(params.id)
+   );
+
+   console.log("params", params);
 
    const [count, setCount] = useState(0);
    const [modalIsOpen, setModalIsOpen] = useState(false);
 
-   function openModal(){
-      if (count!==0){
-         addtoCart(props.product,count);
+   function openModal() {
+      if (count !== 0) {
+         addtoCart(product, count);
          setModalIsOpen(true);
-      } 
+      }
    }
 
    function closeModal(e) {
-      if(e.target.className === "Modal") {
+      if (e.target.className === "Modal") {
          setModalIsOpen(false);
       }
    }
@@ -30,44 +40,55 @@ function ProductDetails(props) {
    return (
       <div className="ProductDetails">
          <div className="ProductDetails-page">
-            <div className="ProductDetails-part1 flex-row">
-               {props.product.image}
-               <div className="ProductDetails-part1-productDetails flex-column">
-                  <h1>{props.product.name}</h1>
-                  <p>{props.product.desc}</p>
-                  <h2>${props.product.price}</h2>
-                  <div className="ProductDetails-part1-productDetails-btns flex-row">
-                     <button className="countBtn" onClick={() => setCount(count + 1)}> {count} </button>;
-                     <Button size="small" variant="contained" bgcolor="orange" onClick={()=>openModal()}> ADD TO CART </Button>
-                     {(modalIsOpen)&&<div className="Modal" id="modal" onClick={(e) => closeModal(e)}>
-                        <SummaryModal setModalIsOpen={props.setModalIsOpen} />
-                     </div> }
+            <div className="ProductDetails-part1">
+               <picture>
+                  <source media="(max-width: 768px)" srcSet={product.imagePathTablet}  />
+                  <source media="(max-width: 600px)" srcSet={product.imagePathMobile} />
+                  <img src={product.imagePathDesktop} alt={product.name} />
+               </picture>
+               <div className="ProductDetails-part1-content flex-column">
+                  <h1 className="productName">{product.name}</h1>
+                  <p className="productDesc">{product.desc}</p>
+                  <h2>${product.price}</h2>
+                  <div className="ProductDetails-part1-content-btns flex-row wrap">
+                     <button className="countBtn" onClick={() => setCount(count + 1)}>{count}</button>
+                     <Button size="small" variant="contained" bgcolor="orange" onClick={() => openModal()}> ADD TO CART </Button>
+                     {modalIsOpen && (
+                        <div className="Modal" id="modal" onClick={(e) => closeModal(e)}>
+                           <SummaryModal setModalIsOpen={props.setModalIsOpen}/>
+                           {/* <SummaryModal {...modalIsOpen}/> */}
+                        </div>
+                     )}
                   </div>
                </div>
             </div>
             <div className="ProductDetails-part2 wrap flex-row">
-               <div className="features flex-column">
-                  <h1>FEATURES</h1>
-                  <p> As the headphones all others are measured against, the XX99 Mark I demonstrates over five decades of audio expertise, redefining the critical listening experience. This pair of closed-back headphones are made of industrial, aerospace-grade materials to emphasize durability at a relatively light weight of 11 oz. </p>
-                  <p> From the handcrafted microfiber ear cushions to the robust metal headband with inner damping element, the components work together to deliver comfort and uncompromising sound. Its closed-back design delivers up to 27 dB of passive noise cancellation, reducing resonance by reflecting sound to a dedicated absorber. For connectivity, a specially tuned cable is included with a balanced gold connector. </p>
-               </div> 
-               <InTheBox />                
+               <Features />
+               <InTheBox />
             </div>
-            <div className="ProductDetails-part3-images flex-row">
+            <AboutUs /> 
+             {/* <ProductImages product={product} /> */}
+            {/* <div className="ProductDetails-part3-images flex-row">
                <div className="ProductDetails-part3-sideLeft flex-column">
-                  <img src="/images/product-xx99-mark-one-headphones/desktop/image-gallery-1.jpg" alt="imageGallery1"/>
-                  <img src="/images/product-xx99-mark-one-headphones/desktop/image-gallery-2.jpg" alt="imageGallery2"/>
-               </div> 
-               <img src="/images/product-xx99-mark-one-headphones/desktop/image-gallery-3.jpg" alt="imageGallery3"/>                 
+                  <img
+                     src="/images/product-xx99-mark-one-headphones/desktop/image-gallery-1.jpg"
+                     alt="imageGallery1"
+                  />
+                  <img
+                     src="/images/product-xx99-mark-one-headphones/desktop/image-gallery-2.jpg"
+                     alt="imageGallery2"
+                  />
+               </div>
+               <img
+                  src="/images/product-xx99-mark-one-headphones/desktop/image-gallery-3.jpg"
+                  alt="imageGallery3"
+               />
             </div>
             <CategoriesShops />
-            <AboutUs />     
-         </div> 
-         {/* {(modalIsOpen)&&<Modal setModalIsOpen={setModalIsOpen} />} */}
-      </div> 
+            <AboutUs />  */}
+         </div>
+      </div>
    );
 }
-
-
 
 export default ProductDetails;
