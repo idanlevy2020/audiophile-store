5a0e6ce232b7db7bd0b88ada5caeb60f2a595e8e
Idan <idanlevy2020@gmail.com>
2021-12-01 15:02:18 +0200

    add margin from page

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -32,84 +32,86 @@ function Checkout() {
 
    return (
       <div className="Checkout">
-         <div className="page checkout-part1">
-         <h1 className="checkout-title">CHECKOUT</h1>
-            <form className="flex-row" onSubmit={(e) => e.preventDefault()}>
-               <div className="checkout-part1">
-                  <div className="col">
-                     <h3 className="subtitle">BILLIG DETAILS</h3>
-                     <div className="box">
-                        <label htmlFor="name"> Name *</label>
-                        <input type="text" id="name" name="name" placeholder="John M. Doe" onChange={(e)=> handleChange({name:e.target.value})} required/>  
-                     </div>
-                     <div className="box">
-                        <label htmlFor="email"> Email Address *</label>
-                        <input type="text" id="email" name="email" placeholder="john@example.com" onChange={e => handleChange({email:e.target.value})} required/>                    
-                     </div>
-                     <div className="box">
-                        <label htmlFor="phone"> Phone Number *</label>
-                        <input type="text" id="phone" name="phone" placeholder="+1 202-555-0136" onChange={e => handleChange({phone:e.target.value})} required/>  
-                     </div>
-                  </div>
-                  <div className="col">
-                     <h3 className="subtitle">SHIPPING INFO</h3>
-                     <div className="box">
-                        <label htmlFor="adr"> Your Address *</label>
-                        <input type="text" id="address" name="address" placeholder="542 W. 15th Street" onChange={(e)=> handleChange({address:e.target.value})} required/>
-                     </div>
-                     <div className="box">
-                        <label htmlFor="zip"> Zip Code *</label>
-                        <input type="text" id="zip" name="zip" placeholder="10001" onChange={(e)=> handleChange({zip:e.target.value})} required/>             
-                     </div>
-                     <div className="box">
-                        <label htmlFor="city"> City *</label>
-                        <input type="text" id="city" name="city" placeholder="New York" onChange={(e)=> handleChange({city:e.target.value})} required/>
-                     </div>
-                     <div className="box">
-                        <label htmlFor="country">Country *</label>
-                        <input type="text" id="country" name="country" placeholder="United States" onChange={(e)=> handleChange({country:e.target.value})} required/> 
-                     </div>
-                  </div>
-               </div>
-               <div className="checkout-part2">
-                  <div className="col">
-                     <h3 className="subtitle">PAYMENT DETAILS</h3>
-                     <div className="box"> 
-                        <label htmlFor="fname">Accepted Cards</label>
-                        <div className="icon-container">
-                           <i className="fa fa-cc-visa" style={{color: "navy"}}></i>
-                           <i className="fa fa-cc-amex" style={{color: "blue"}}></i>
-                           <i className="fa fa-cc-mastercard" style={{color: "red"}}></i>
-                           <i className="fa fa-cc-discover" style={{color: "orange"}}></i>
+         <div className="page-content">
+            <div className="checkout-part1">
+            <h1 className="checkout-title">CHECKOUT</h1>
+               <form className="flex-row" onSubmit={(e) => e.preventDefault()}>
+                  <div className="checkout-part1">
+                     <div className="col">
+                        <h3 className="subtitle">BILLIG DETAILS</h3>
+                        <div className="box">
+                           <label htmlFor="name"> Name *</label>
+                           <input type="text" id="name" name="name" placeholder="John M. Doe" onChange={(e)=> handleChange({name:e.target.value})} required/>  
+                        </div>
+                        <div className="box">
+                           <label htmlFor="email"> Email Address *</label>
+                           <input type="text" id="email" name="email" placeholder="john@example.com" onChange={e => handleChange({email:e.target.value})} required/>                    
+                        </div>
+                        <div className="box">
+                           <label htmlFor="phone"> Phone Number *</label>
+                           <input type="text" id="phone" name="phone" placeholder="+1 202-555-0136" onChange={e => handleChange({phone:e.target.value})} required/>  
                         </div>
                      </div>
-                     <div className="box">
-                        <label htmlFor="cname">Name on Card *</label>
-                        <input type="text" id="cname" name="cardname" placeholder="John More Doe" onChange={e => handleChange({cardname:e.target.value})} required/>              
-                     </div>
-                     <div className="box">
-                        <label htmlFor="ccnum">Credit card number *</label>
-                        <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444" onChange={e => handleChange({cardnumber:e.target.value})} required/>  
-                     </div>
-                     <div className="box">
-                        <label htmlFor="expmonth">Exp Month *</label>
-                        <input type="text" id="expmonth" name="expmonth" placeholder="September" onChange={e => handleChange({expmonth:e.target.value})} required/>  
-                     </div>
-                     <div className="box">
-                        <label htmlFor="expyear">Exp Year *</label>
-                        <input type="text" id="expyear" name="expyear" placeholder="2026" onChange={e => handleChange({expyear:e.target.value})} required/>  
-                     </div>
-                     <div className="box">
-                        <label htmlFor="cvv">CVV *</label>
-                        <input type="text" id="cvv" name="cvv" placeholder="352" onChange={e => handleChange({cvv:e.target.value})} required/>  
+                     <div className="col">
+                        <h3 className="subtitle">SHIPPING INFO</h3>
+                        <div className="box">
+                           <label htmlFor="adr"> Your Address *</label>
+                           <input type="text" id="address" name="address" placeholder="542 W. 15th Street" onChange={(e)=> handleChange({address:e.target.value})} required/>
+                        </div>
+                        <div className="box">
+                           <label htmlFor="zip"> Zip Code *</label>
+                           <input type="text" id="zip" name="zip" placeholder="10001" onChange={(e)=> handleChange({zip:e.target.value})} required/>             
+                        </div>
+                        <div className="box">
+                           <label htmlFor="city"> City *</label>
+                           <input type="text" id="city" name="city" placeholder="New York" onChange={(e)=> handleChange({city:e.target.value})} required/>
+                        </div>
+                        <div className="box">
+                           <label htmlFor="country">Country *</label>
+                           <input type="text" id="country" name="country" placeholder="United States" onChange={(e)=> handleChange({country:e.target.value})} required/> 
+                        </div>
                      </div>
                   </div>
-               </div>
-               <div className="checkout-part3">
-                  <Summary flagFormFilled={flagFormFilled}/>
-               </div>  
-            </form>  
-         </div>  
+                  <div className="checkout-part2">
+                     <div className="col">
+                        <h3 className="subtitle">PAYMENT DETAILS</h3>
+                        <div className="box"> 
+                           <label htmlFor="fname">Accepted Cards</label>
+                           <div className="icon-container">
+                              <i className="fa fa-cc-visa" style={{color: "navy"}}></i>
+                              <i className="fa fa-cc-amex" style={{color: "blue"}}></i>
+                              <i className="fa fa-cc-mastercard" style={{color: "red"}}></i>
+                              <i className="fa fa-cc-discover" style={{color: "orange"}}></i>
+                           </div>
+                        </div>
+                        <div className="box">
+                           <label htmlFor="cname">Name on Card *</label>
+                           <input type="text" id="cname" name="cardname" placeholder="John More Doe" onChange={e => handleChange({cardname:e.target.value})} required/>              
+                        </div>
+                        <div className="box">
+                           <label htmlFor="ccnum">Credit card number *</label>
+                           <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444" onChange={e => handleChange({cardnumber:e.target.value})} required/>  
+                        </div>
+                        <div className="box">
+                           <label htmlFor="expmonth">Exp Month *</label>
+                           <input type="text" id="expmonth" name="expmonth" placeholder="September" onChange={e => handleChange({expmonth:e.target.value})} required/>  
+                        </div>
+                        <div className="box">
+                           <label htmlFor="expyear">Exp Year *</label>
+                           <input type="text" id="expyear" name="expyear" placeholder="2026" onChange={e => handleChange({expyear:e.target.value})} required/>  
+                        </div>
+                        <div className="box">
+                           <label htmlFor="cvv">CVV *</label>
+                           <input type="text" id="cvv" name="cvv" placeholder="352" onChange={e => handleChange({cvv:e.target.value})} required/>  
+                        </div>
+                     </div>
+                  </div>
+                  <div className="checkout-part3">
+                     <Summary flagFormFilled={flagFormFilled}/>
+                  </div>  
+               </form>  
+            </div>  
+         </div>
       </div>
    )
 }
